bb166af9125b0fef4c354c519f70bee4fb2e77d7
Checkinoler <checkinoler@protonmail.com>
2016-07-05 14:37:23 +0800

    0.0.3
    修复了点击后无反应的问题

--- src/scenes/StartScene.js
+++ src/scenes/StartScene.js
@@ -1,9 +1,19 @@
 "use strict";
 
+var Listener = cc.EventListener.create({
+    event: cc.EventListener.TOUCH_ONE_BY_ONE,
+    onTouchBegan: function () {
+        alert("Entering GameScene...");
+        cc.director.runScene(new GameScene());
+    }
+});
+
 var StartLayer = cc.Layer.extend({
     ctor: function () {
         this._super();
         var size = cc.winSize;
+
+        cc.eventManager.addListener(Listener, this);
 
         return true;
     }
@@ -15,9 +25,5 @@ var StartScene = cc.Scene.extend({
 
         var layer = new StartLayer();
         this.addChild(layer);
-    },
-    onTouchBegan: function (touch, event) {
-        alert("Changing to GameScene...");
-        cc.director.runScene(new GameScene());
     }
 });
